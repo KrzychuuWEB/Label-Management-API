bed654a356354d6931a1c822ffd65761289b1ee0
Krzysztof <krzychuuweb@gmail.com>
2023-07-21 18:10:05 +0200

    Empty app with docker

---
+++ src/main/java/pl/krzychuuweb/labelapp/LabelappApplication.java
@@ -0,0 +1,13 @@
+package pl.krzychuuweb.labelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelappApplication.class, args);
+	}
+
+}
