fdaece303ffb2e01513d8824118b669181d58432
Krzysztof <krzychuuweb@gmail.com>
2023-10-15 13:01:41 +0200

    Create integration test for AuthController

--- src/test/java/pl/krzychuuweb/labelapp/auth/AuthControllerIT.java
+++ src/test/java/pl/krzychuuweb/labelapp/auth/AuthControllerIT.java
@@ -1,9 +1,21 @@
 package pl.krzychuuweb.labelapp.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.MvcResult;
 import pl.krzychuuweb.labelapp.IntegrationTestConfig;
+import pl.krzychuuweb.labelapp.auth.dto.AuthDTO;
+import pl.krzychuuweb.labelapp.auth.dto.LoginDTO;
+import pl.krzychuuweb.labelapp.user.UserFacade;
+import pl.krzychuuweb.labelapp.user.dto.UserCreateDTO;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class AuthControllerIT extends IntegrationTestConfig {
 
@@ -13,5 +25,42 @@ class AuthControllerIT extends IntegrationTestConfig {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private UserFacade userFacade;
 
+    @Test
+    @Transactional
+    void should_return_jwt_token() throws Exception {
+        UserCreateDTO userCreateDTO = new UserCreateDTO("firstName", "email@email.com", "password12345");
+        userFacade.addUser(userCreateDTO);
+        LoginDTO loginDTO = new LoginDTO(userCreateDTO.email(), userCreateDTO.password());
+
+        MvcResult result = mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDTO))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        AuthDTO response = objectMapper.readValue(result.getResponse().getContentAsString(), AuthDTO.class);
+
+        assertThat(response.token()).isNotNull();
+    }
+
+    @Test
+    @Transactional
+    void should_register_user() throws Exception {
+        UserCreateDTO userCreateDTO = new UserCreateDTO("firstName", "email@email.com", "password12345");
+
+        MvcResult result = mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateDTO))
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        AuthDTO response = objectMapper.readValue(result.getResponse().getContentAsString(), AuthDTO.class);
+
+        assertThat(response.token()).isNotNull();
+    }
 }
